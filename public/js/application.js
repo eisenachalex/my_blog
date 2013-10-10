c8ff550bce0065a2eb5ad29e4736af467561e5e1
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-10 18:53:54 -0500

    almost there

---
+++ public/js/application.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $("a#delete").click(function(event) {
+      event.preventDefault();
+      var id =  $(this).attr("name");
+      $.get("delete_post/" + id, function() {
+        $("#" + id).fadeOut();
+      });
+  });
+});
+
+
+$(document).ready(function() {
+  $("a#edit").click(function(event) {
+      event.preventDefault();
+      var id =  $(this).attr("name");
+      $.get("edit_post/" + id, function(data) {
+        $("#" + id).html(data);
+      });
+  });
+});
+
+$(document).ready(function() {
+  $("form#edit").submit(function(event) {
+      event.preventDefault();
+      var id =  $("#post_id").attr("class");
+      $.post("/save_post", $(this).serialize(), function(data) {
+        $('#' + id).html(data);
+      });
+  });
+});
